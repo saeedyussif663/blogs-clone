628a65b4d8300fc46795965492aa30e5beb61265
saeedyussif663 <saeedyussif663@gmail.com>
2023-08-09 21:54:00 +0100

    added a loader component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react'
 
 import NavBar from './Components/NavBar';
 import Form from './Components/Form';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
 import SingleBlog from './Components/SingleBlog';
+import Loader from './Components/Loader'
 
 import './index.css';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <div className="container">
       <NavBar />
+      {isLoading && <Loader/>}
       <Routes>
         <Route   path='/' element={<Home />}/>
         <Route path='/blog/:id'  element={<SingleBlog/>} />
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,12 +4,16 @@ import { Link } from "react-router-dom"
 const NavBar = () => {
     return (
         <div className="nav-container">
+            <nav>
             <h3>Bloggg</h3>
-            <ul>
-                <Link to='/'><button>Blogs</button></Link>
-                <Link to='/addnew'><button>Create Blog</button></Link>
-            </ul>
-            <button className="menu">Menu</button>
+                <div className="bar">
+                    <ul>
+                    <Link to='/'><button>Blogs</button></Link>
+                    <Link to='/addnew'><button>Create Blog</button></Link>
+                    </ul>
+                </div>
+            </nav>
+                <button className="menu">Menu</button>
         </div>
     )
 }
