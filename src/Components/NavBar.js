e71e1f001d0f9f7a7304edfc97e993e15eca7ae2
saeedyussif663 <saeedyussif663@gmail.com>
2023-08-09 23:36:21 +0100

    mobile navigation finsihed + toggle

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,19 +1,34 @@
-import { Link } from "react-router-dom"
- 
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
+    const [isShowing, setIsShowing] = useState(true);
+
+    const toggleMenu = () => {
+        setIsShowing(prevIsShowing => {
+            console.log('working');
+            return !prevIsShowing
+        });
+    }
+
     return (
         <div className="nav-container">
             <h3>Bloggg</h3>
+            {isShowing && (
                 <div className="bar">
+                    <div className="mobile-nav">
                     <ul>
-                    <Link to='/'><button>Blogs</button></Link>
-                    <Link to='/addnew'><button>Create Blog</button></Link>
-                    </ul>
+                        <Link to='/'><button>Blogs</button></Link>
+                        <Link to='/addnew'><button>Create Blog</button></Link>
+                        </ul>
+                    </div>
                 </div>
-                <button className="menu">Menu</button>
+            )}
+            <button className="menu" onClick={toggleMenu}>
+                Menu
+            </button>
         </div>
-    )
+    );
 }
 
-export default NavBar
+export default NavBar;
